95b23d14dc2b5e430d2f622f6887ca80054a518c
William Blair <60581672+williamblair333@users.noreply.github.com>
2021-12-12 14:54:41 -0500

    Add files via upload
    This is for the Konsave project

---
+++ konsave-profile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#The purpose of this script is to automate profile creation and export of KDE user
+#configurations using the Konsave project (https://github.com/Prayag2/konsave)
+#################################################################################
+
+source '/opt/src/shell/date-time.config'
+
+rm ${HOME}/.config/konsave/*.knsv
+${HOME}/.local/bin/konsave -w
+echo "WIPE"
+
+ext='knsv'
+export_path=${HOME}/.config/konsave
+echo $export_path
+export_path=${export_pathvar%?}
+echo $export_path
+
+username=$(whoami)
+hostname=$(cat /etc/hostname)
+filename=$hostname-$username-$year-$month-$day
+
+echo $username
+echo $hostname
+echo $filename
+
+${HOME}/.local/bin/konsave -s $filename
+${HOME}/.local/bin/konsave -e $filename
+echo "Konsave: Moving" $filename.$ext "to" $export_path
+echo "here" $export_path
+mv ${HOME}/$filename.$ext ${HOME}/.config/konsave/$filename.$ext
