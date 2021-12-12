9972e159ef4653f2b46cdc583e8887a3608979b7
William Blair <60581672+williamblair333@users.noreply.github.com>
2021-12-12 17:23:26 -0500

    Update konsave-profile.sh

--- konsave-profile.sh
+++ konsave-profile.sh
@@ -8,24 +8,18 @@ source '/path/to/file/date-time.config'
 
 rm ${HOME}/.config/konsave/*.knsv
 ${HOME}/.local/bin/konsave -w
-echo "WIPE"
 
 ext='knsv'
 export_path=${HOME}/.config/konsave
-echo $export_path
-export_path=${export_pathvar%?}
-echo $export_path
 
 username=$(whoami)
 hostname=$(cat /etc/hostname)
 filename=$hostname-$username-$year-$month-$day
 
-echo $username
-echo $hostname
-echo $filename
-
 ${HOME}/.local/bin/konsave -s $filename
 ${HOME}/.local/bin/konsave -e $filename
+
 echo "Konsave: Moving" $filename.$ext "to" $export_path
 echo "here" $export_path
+
 mv ${HOME}/$filename.$ext ${HOME}/.config/konsave/$filename.$ext
