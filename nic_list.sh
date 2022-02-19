53de928f44168ab76c838fe266aa5eab1ce6b135
William Blair <60581672+williamblair333@users.noreply.github.com>
2022-02-19 09:57:06 -0500

    Create nic_list.sh

---
+++ nic_list.sh
@@ -0,0 +1,14 @@
+#awk NR== refers to which line number to return / print
+#grep -A refers to returning result plus x lines after -B before -C before and after
+#echo "${variable//<character>}") will remove the character listed and optionally all characters after
+nic_up=$(ip address | grep 'state UP'| awk '{print $2}')
+
+for name in $nic_up
+do
+   nic_name=$(ip address | grep -A 2 "$name" | awk '{print $2}' | awk 'NR==1')
+   nic_name=$(echo "${nic_name//:}")
+   nic_mac=$(ip address | grep -A 2 "$name" | awk '{print $2}' | awk 'NR==2')
+   nic_ip=$(ip address | grep -A 2 "$name" | awk '{print $2}' | awk 'NR==3')
+   nic_ip=$(echo "${nic_ip//\/*}")   
+   echo $nic_name","$nic_mac","$nic_ip
+done
