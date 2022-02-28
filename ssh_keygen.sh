fe10cf9962fdc5ded8a27a318ec3991078093716
William Blair <60581672+williamblair333@users.noreply.github.com>
2022-02-27 21:31:43 -0500

    Update ssh_keygen.sh

--- ssh_keygen.sh
+++ ssh_keygen.sh
@@ -25,6 +25,7 @@
 
 user_name="$1"
 dir_ssh="$2"
+ip_addr="192.168.1.13"
 
 if [ -z "$user_name" ]; then
 	user_name=$(whoami)
@@ -39,16 +40,19 @@ key_name=id_rsa
 ssh-keygen -f $HOME/.ssh/$key_name -t rsa -N ''
 
 #check if .ssh exists. If not, create the directory and copy the public key 
-if ssh $(whoami)@192.168.1.13 '[ -d $HOME/.ssh ]'
+if ssh $(whoami)@$ip_addr '[ -d $HOME/.ssh ]'
 then
 {
     echo 'directory already exists'
-    scp ${HOME}/.ssh/$(whoami)_id_rsa.pub $(whoami)@192.168.1.13:${HOME}/.ssh/$(whoami)_id_rsa.pub
+    scp ${HOME}/.ssh/$key_name $(whoami)@$ip_addr:${HOME}/.ssh/$key_name
 }
 else
 {
-    ssh $(whoami)@192.168.1.13 "mkdir $HOME/.ssh"
-    scp ${HOME}/.ssh/$(whoami)_id_rsa.pub $(whoami)@192.168.1.13:${HOME}/.ssh/$(whoami)_id_rsa.pub
+    ssh $(whoami)@$ip_addr "mkdir $HOME/.ssh"
+    scp ${HOME}/.ssh/$key_name $(whoami)@$ip_addr:${HOME}/.ssh/$key_name
 }
 fi
+
+ssh-copy-id -i ${HOME}/.ssh/$key_name $(whoami)@$ip_addr
+
 #################################################################################
