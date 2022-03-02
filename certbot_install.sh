f716dee80c184a8326fe854ff17121f2d22f3f1e
William Blair <60581672+williamblair333@users.noreply.github.com>
2022-03-02 10:31:19 -0500

    Update certbot_install.sh

--- certbot_install.sh
+++ certbot_install.sh
@@ -25,7 +25,7 @@ sudo apt-get update
 sudo apt-get install software-properties-common
 #sudo add-apt-repository universe
 #sudo add-apt-repository ppa:certbot/certbot
-apt-get install software-properties-common certbot
+sudo apt-get install software-properties-common certbot
 sudo apt-get update
 
 #Generate A Certificate
