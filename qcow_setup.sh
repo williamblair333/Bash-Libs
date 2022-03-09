120ea8bca9561c9249cd05b42f93fa11bded1a72
William Blair <60581672+williamblair333@users.noreply.github.com>
2022-03-08 23:24:28 -0500

    Create qcow_setup.sh

---
+++ qcow_setup.sh
@@ -0,0 +1,94 @@
+
+#!/bin/env bash
+
+#set -x
+#trap read debug
+
+#################################################################################
+#
+#Run example: ./ssh_keygen.sh -u libvirt-qemu -n NixOS-01 -d NixOS-01 -p /opt/lib/libvirt/images -s 1G -f qcow2 -c /opt/lib/libvirt/iso -i firmware-11.2.0-amd64-netinst.iso -v 1 -m 2048
+#File:        ssh_keygen.sh
+#Date:        2022MAR08
+#Author:      William Blair
+#Contact:     williamblair333@gmail.com
+#Tested on:   Debian 11
+#To test:     Ubuntu 20+
+#
+#This script is intended to do the following:
+#
+#- helper script to setup (usually) qcow image in order to quickly get virtual
+#- machines up and running
+#################################################################################
+
+user_name=libvirt-qemu
+image_name=NixOS-01
+image_dir=$image_name
+image_path=/opt/lib/libvirt/images
+image_size=1G
+image_format=qcow2
+cdrom_path=/opt/lib/libvirt/iso
+cdrom_iso=firmware-11.2.0-amd64-netinst.iso
+vcpus=1
+memory=2048
+#################################################################################
+
+while getopts u:d:p:s:f:c:i:v:m: flag
+do
+    case "${flag}" in
+        u) user_name=${OPTARG};;
+        u) image_name=${OPTARG};;
+        d) image_dir=${OPTARG};;
+        p) image_path=${OPTARG};;
+        s) image_size=${OPTARG};;
+        f) image_format=${OPTARG};;
+        c) cdrom_path=${OPTARG};;
+        i) cdrom_iso=${OPTARG};;
+        v) vcpus=${OPTARG};;
+        m) memory=${OPTARG};;
+    esac
+done
+#################################################################################
+
+#check if the qcow image directory exists
+dir_chk=$(ls -a $image_path/$image_dir)
+
+if [ "$image_dir" != "$dir_chk" ]; then
+    mkdir $image_path/$image_dir
+else
+{
+    echo "directory already exists"
+}
+fi
+#################################################################################
+
+su -s /bin/bash $user_name
+#################################################################################
+
+#check if the qcow image exists already
+img_chk=$(ls -a $image_path/$image_dir/$image_name.$image_format)
+
+if [ "$image_path/$image_dir/$image_name.$image_format" != "$img_chk" ]; then
+    qemu-img create -f $image_format $image_path/$image_dir/$image_name.$image_format $image_size
+
+    chown $user_name $image_path/$image_dir/$image_name.$image_format
+    chgrp $user_name $image_path/$image_dir/$image_name.$image_format
+    chown $user_name $cdrom_path/$cdrom_iso
+    chgrp $user_name $cdrom_path/$cdrom_iso
+
+    #Build the qcow / vm 
+    virt-install \
+    --name=$image_name \
+    --vcpus=$vcpus \
+    --memory=$memory \
+    --cdrom=$cdrom_path/$cdrom_iso \
+    --disk path=$image_path/$image_dir/$image_name.$image_format
+    #--os-variant ubuntu18.04
+else
+{
+    echo "image already exists. quitting"
+    exit N
+}
+fi
+#################################################################################
+
+echo Now go set up the pool....
