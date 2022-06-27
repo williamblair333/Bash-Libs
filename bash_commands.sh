918f2c0d4a1b68ac36be2338ee8269df2a6dbe2e
William Blair <60581672+williamblair333@users.noreply.github.com>
2022-06-27 10:52:52 -0400

    Update bash_commands.sh

--- bash_commands.sh
+++ bash_commands.sh
@@ -14,3 +14,6 @@ sudo systemctl status service-name | grep Active | awk '{print $6 "|" $7}'
 
 #use sed to grep for a string and make changes before or after that line -1, +2 etc..
 sed -i "$(( $(sed -n "/string_on_a_line/"= /path/to/file.conf) +- int )) s/find_string/replace_string/g" /path/to/file.conf
+
+#create multiple directories, useful for building a skeleton quickly
+mkdir -p htg/{articles/{new,rewrites},images,notes,done}
