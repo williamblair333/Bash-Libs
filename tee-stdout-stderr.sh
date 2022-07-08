28950ec4ca7799ee7ca857e12899812432ea0a21
William Blair <60581672+williamblair333@users.noreply.github.com>
2022-07-08 09:50:43 -0400

    Update tee-stdout-stderr.sh

--- tee-stdout-stderr.sh
+++ tee-stdout-stderr.sh
@@ -1,2 +1,6 @@
 #create two separate files, and display on screen and combine all output to one file
 cmd | tee stdout.log 3>&1 1>&2 2>&3 | tee stderr.log &> all.log
+
+#less confusing, suppress errors, create two separate files with console output 
+#tee -a to append
+cmd 2>/tmp/stderr.log | tee /tmp/stdout.log
